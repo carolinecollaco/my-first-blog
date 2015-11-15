e82c2c5df44bfb53f61669c6fb7e6856a29eaa41
Caroline.Collaco <carolinecollaco@gmail.com>
2015-11-15 13:20:33 -0200

    Changed ths resquests post_list.

--- blog/views.py
+++ blog/views.py
@@ -6,8 +6,9 @@ from .forms import PostForm
 from django.shortcuts import redirect
 
 def post_list(request):
+    #posts = Post.objects.order_by('published_date')
     posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
-    return render(request, 'blog/post_list.html', {})
+    return render(request, 'blog/post_list.html', {'posts': posts})
 
 def post_detail(request, pk):
     post = get_object_or_404(Post, pk=pk)
